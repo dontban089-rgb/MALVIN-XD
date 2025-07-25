a0c58bf13d607049926b73c543b166258b4e879d
dontban089-rgb <dontban089@gmail.com>
2025-07-25 23:05:24 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~Jnt1XRZR#MQBhlbGFvPdY4T9hfOzVVcj7xtY_N9Vhoqq0fARRVgw",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "27783583068", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
